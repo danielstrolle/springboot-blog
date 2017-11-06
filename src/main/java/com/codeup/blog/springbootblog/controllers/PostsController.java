e94fabc777fd407f3c3da337a8b177d9705c32b5
danielstrolle <danielstrolle@gmail.com>
2017-11-06 09:06:16 -0600

    Complete the views exercises
    * Update postsController
    
    * update diceroller to use 3 rolls and present them to the user and tell them if their guess matched any of the rolls
    
    * create a posts model
    
    * create fragments for header footer and navbar
    
    * create html for index and show, showing all or just one post

--- src/main/java/com/codeup/blog/springbootblog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/springbootblog/controllers/PostsController.java
@@ -15,20 +15,24 @@ public class PostsController {
     @GetMapping("/posts")
     public String postsIndex(Model model) {
         //3 different posts
-        Posts post = new Posts();
-        post.setTitle("a title");
-        post.setBody("a body");
+        Posts post = new Posts("a title", "a body");
+        // IF WE DONT MAKE A CUSTOM CONSTRUCTOR, WE haVE TO USE SETS. BETTER TO MAKE A CUSTOM SO THAT A USER HAS TO PASS THE FIELDS WERE LOOKING FOR IN ORDER TO MAKE THE OBJECT
+//        post.setTitle("a title");
+//        post.setBody("a body");
 
-        Posts post2 = new Posts();
-        post2.setTitle("2 title");
-        post2.setBody("2 body");
+        Posts post2 = new Posts("2 title", "2 body");
+//        post2.setTitle("2 title");
+//        post2.setBody("2 body");
 
-        Posts post3 = new Posts();
-        post3.setTitle("3 title");
-        post3.setBody("3 body");
+        Posts post3 = new Posts("3 title", "3 body");
+//        post3.setTitle("3 title");
+//        post3.setBody("3 body");
 
         // create an array list
         ArrayList<Object> posts = new ArrayList<>();
+
+        // IF WE CREATE THE ARRAY LIST FIRST WE CAN CPASS OBJECTS AS WE CREATE THEM
+//        posts.add(new Posts("a title", "a body"));
 
         // add my post objects to the list
         posts.add(post);
@@ -45,9 +49,9 @@ public class PostsController {
     @GetMapping("/posts/{id}")
     public String postsID(Integer id, Model model){
 
-        Posts post = new Posts();
-        post.setTitle("a title");
-        post.setBody("a body");
+        Posts post = new Posts("a title", "a body");
+//        post.setTitle("a title");
+//        post.setBody("a body");
 
         model.addAttribute("post", post);
 
--- src/main/java/com/codeup/blog/springbootblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/blog/springbootblog/controllers/RollDiceController.java
@@ -11,25 +11,40 @@ import java.util.Random;
 public class RollDiceController {
 
     @GetMapping("/roll-dice")
-    public String guess() {
-         return "diceRollGuess";
+    public String showPage() {
+        return "diceRollGuess";
     }
 
     @GetMapping("/roll-dice/{guess}")
-    public String userGuess (@PathVariable int guess, Model viewModel){
-        if (guess>6 || guess < 1) {
+    public String userGuess(@PathVariable int guess, Model viewModel) {
+        if (guess > 6 || guess < 1) {
             viewModel.addAttribute("error", "Dice only has 6 sides :p");
         }
         Random rand = new Random();
-        int  n = rand.nextInt(6) + 1;
-        String match = "";
+        int n = rand.nextInt(6) + 1;
+        int n2 = rand.nextInt(6) + 1;
+        int n3 = rand.nextInt(6) + 1;
+
+        int counter = 0;
         if (n == guess) {
-            match= "You got it right!";
-        } else {
-            match="You got it wrong :(";
+            counter += 1;
+        } else if (n2 == guess) {
+            counter += 1;
+        } else if (n3 == guess) {
+            counter += 1;
         }
 
-        viewModel.addAttribute("diceroll", n);
+        String match;
+
+        if (counter > 0) {
+            match = "You got " + counter + " out of 3 right!";
+        } else {
+            match = "Sorry, you didnt get any right :(";
+        }
+
+        viewModel.addAttribute("diceroll1", n);
+        viewModel.addAttribute("diceroll2", n2);
+        viewModel.addAttribute("diceroll3", n3);
         viewModel.addAttribute("guess", guess);
         viewModel.addAttribute("match", match);
 
